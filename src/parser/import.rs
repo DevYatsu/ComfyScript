1aa1624c1175c8ceb7ceb3fa98928a57c7d8415b
Yatsu <yatsu.dev@gmail.com>
2023-09-28 22:24:59 +0200

    code opti

--- src/parser/import.rs
+++ src/parser/import.rs
@@ -2,7 +2,7 @@ use nom::{
     character::complete::{alphanumeric1, multispace0},
     error::VerboseError,
     multi::separated_list1,
-    IResult,
+    IResult, combinator::opt,
 };
 use nom_supreme::tag::complete::tag;
 
@@ -35,7 +35,26 @@ pub fn parse_import(i: &str) -> IResult<&str, ASTNode, VerboseError<&str>> {
 
 fn parse_import_ids(input: &str) -> IResult<&str, ImportSpecifier, VerboseError<&str>> {
     let (input, _) = multispace0(input)?;
-    let (input, name) = alpha_not_reserved(input)?;
+    let (input, imported_name) = alpha_not_reserved(input)?;
+    let (input, _) = multispace0(input)?;
+    let (input, opt_val) = opt(tag("as"))(input)?;
+
+    if opt_val != None {
+        let (input, _) = multispace0(input)?;
+        let (input, local_name) = alpha_not_reserved(input)?;
+
+            return Ok((
+        input,
+        ImportSpecifier {
+            local: Identifier {
+                name: local_name.to_owned(),
+            },
+            imported: Identifier {
+                name: imported_name.to_owned(),
+            },
+        },
+    ))
+    }
 
     //todo!! add support for "as" keyword to rename import locally
 
@@ -43,10 +62,10 @@ fn parse_import_ids(input: &str) -> IResult<&str, ImportSpecifier, VerboseError<
         input,
         ImportSpecifier {
             local: Identifier {
-                name: name.to_owned(),
+                name: imported_name.to_owned(),
             },
             imported: Identifier {
-                name: name.to_owned(),
+                name: imported_name.to_owned(),
             },
         },
     ))
