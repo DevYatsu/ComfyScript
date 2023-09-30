bda0e6bd1083064c7e059f25f002c5ed204393de
Yatsu <yatsu.dev@gmail.com>
2023-09-30 19:35:23 +0200

    code opti

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,5 @@
 use custom_error::custom_error;
 
-custom_error! {pub ParsingError
-    Custom{string: String} = "{string}",
-    ImportError{source: ImportError} = "{source}",
-}
 custom_error! {pub ImportError
     Custom{string: String} = "{string}",
     ExpectedSpecifier{code: String, line: String} = "Import Error: Expected a valid import specifier \n At {code} \n line {line}",
