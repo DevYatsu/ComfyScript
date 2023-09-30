2d25c54960735b4bcabd59b3658e469f5af41ec5
Yatsu <yatsu.dev@gmail.com>
2023-09-30 21:40:37 +0200

    code opti

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -1,22 +1,21 @@
 use crate::reserved_keywords::RESERVED_KEYWORD;
 use nom::{
-    character::complete::{alphanumeric1, char as parse_char},
+    character::complete::{alphanumeric1},
     error::VerboseError,
-    Err, IResult,
+    Err, IResult, bytes::complete::tag, branch::alt,
 };
 
 use super::Span;
 
 pub fn parse_identifier(i: Span) -> IResult<Span, String, VerboseError<Span>> {
-    let (mut i, word) = alphanumeric1(i)?;
+    let (mut i, word) = alt((alphanumeric1, tag("_")))(i)?;
     let mut word = word.fragment().to_string();
-
     loop {
-        match parse_char::<Span, VerboseError<Span>>('_')(i) {
-            Ok((input, c)) => {
-                word.push(c);
+        match alt((alphanumeric1::<Span, VerboseError<Span>>, tag("_")))(i) {
+            Ok((input, s)) => {
+                word.push_str(s.fragment());
 
-                match alphanumeric1::<Span, VerboseError<Span>>(input) {
+                match alt((alphanumeric1::<Span, VerboseError<Span>>, tag("_")))(input) {
                     Ok((input, w)) => {
                         word.push_str(w.fragment());
                         i = input;
