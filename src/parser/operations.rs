a9f2438d88743905aaa741783342545fe4a23128
Yatsu <yatsu.dev@gmail.com>
2023-10-01 11:31:12 +0200

    operators parser updated

--- src/parser/operations.rs
+++ src/parser/operations.rs
@@ -1,4 +1,6 @@
-use nom::{character::complete::one_of, error::VerboseError, IResult};
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::one_of, error::VerboseError, IResult,
+};
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Operator {
@@ -8,20 +10,32 @@ pub enum Operator {
     Divide,
     Equal, // ==
     Not,   // !=
+    Modulo,
 }
 
 pub fn parse_operator(i: &str) -> IResult<&str, Operator, VerboseError<&str>> {
     // one_of matches one of the characters we give it
-    let (i, t) = one_of("+-*/=")(i)?;
+    let (i, t) = alt((
+        tag("+"),
+        tag("-"),
+        tag("*"),
+        tag("/"),
+        tag("=="),
+        tag("%"),
+        tag("!="),
+    ))(i)?;
 
     Ok((
         i,
         match t {
-            '+' => Operator::Plus,
-            '-' => Operator::Minus,
-            '*' => Operator::Times,
-            '/' => Operator::Divide,
-            '=' => Operator::Equal,
+            "+" => Operator::Plus,
+            "-" => Operator::Minus,
+            "*" => Operator::Times,
+            "/" => Operator::Divide,
+            "%" => Operator::Modulo,
+            "==" => Operator::Equal,
+            "!=" => Operator::Not,
+
             _ => unreachable!(),
         },
     ))
