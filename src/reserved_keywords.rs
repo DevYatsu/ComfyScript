61bfb80a1c281d2b7e44a3d1c22a5cbe2b692e91
Yatsu <yatsu.dev@gmail.com>
2023-10-01 12:26:08 +0200

    syntax modifications add

--- src/reserved_keywords.rs
+++ src/reserved_keywords.rs
@@ -1,4 +1,4 @@
-pub const RESERVED_KEYWORD: [&str; 13] = [
-    "true", "false", "nil", "let", "var", "import", "export", "fn", "for", "in", "while", "as",
-    "class",
+pub const RESERVED_KEYWORD: [&str; 18] = [
+    "true", "false", "nil", "let", "var", "import", "export", "from", "anon", "fn", "return", "for", "in", "while", "as",
+    "class", "define", "method", 
 ];
