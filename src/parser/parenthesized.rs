5f08f81faf843aa9ed916a3ee593a73b031386dd
Yatsu <yatsu.dev@gmail.com>
2023-10-11 01:14:56 +0200

    code opti

--- src/parser/parenthesized.rs
+++ src/parser/parenthesized.rs
@@ -9,8 +9,8 @@ use super::composite_types::parse_composite_value;
 use super::operations::parse_binary_operation;
 use super::primitive_values::parse_primitive_value;
 
-pub fn parse_parenthesized(initial_i: Span) -> IResult<Span, Expression, VerboseError<Span>> {
-    let (i, _) = char('(')(initial_i)?;
+pub fn parse_parenthesized(i: Span) -> IResult<Span, Expression, VerboseError<Span>> {
+    let (i, _) = char('(')(i)?;
     let (i, _) = multispace0(i)?;
 
     let (i, expr) = alt((
--- src/parser/primitive_values.rs
+++ src/parser/primitive_values.rs
@@ -5,8 +5,6 @@ pub mod strings;
 
 use nom::{
     branch::alt,
-    character::complete::{char, multispace0},
-    combinator::opt,
     error::VerboseError,
     IResult,
 };
@@ -16,16 +14,5 @@ use crate::parser::{ast::Expression, Span};
 use self::{bool::parse_bool, nil::parse_nil, numbers::parse_number, strings::parse_string};
 
 pub fn parse_primitive_value(i: Span) -> IResult<Span, Expression, VerboseError<Span>> {
-    let (i, open_paren) = opt(char('('))(i)?;
-
-    if let Some(_) = open_paren {
-        let (i, _) = multispace0(i)?;
-        let (i, expr) = alt((parse_string, parse_bool, parse_number, parse_nil))(i)?;
-        let (i, _) = multispace0(i)?;
-        let (i, _) = char(')')(i)?;
-
-        return Ok((i, expr));
-    }
-
     alt((parse_string, parse_bool, parse_number, parse_nil))(i)
 }
