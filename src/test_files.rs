51a5e31ef39401772d395336604bd9d26f964085
Yatsu <yatsu.dev@gmail.com>
2023-10-13 23:02:06 +0200

    rayon used to speed up for high number of files parsing

--- src/test_files.rs
+++ src/test_files.rs
@@ -2,38 +2,36 @@ use std::{error::Error, fs, time::Instant};
 
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 
-pub fn parse_all_files() -> Result<(), Box<dyn Error>> {
-    use crate::exec::exec_script;
+use crate::exec::exec_script;
 
+pub fn parse_all_files() -> Result<(), Box<dyn Error>> {
     let folder_path = "tests/";
     let start_time = Instant::now();
 
-    if let Ok(entries) = fs::read_dir(folder_path) {
-        let files: Vec<_> = entries
-            .filter_map(|entry| entry.ok().map(|e| e.path()))
-            .collect();
+    let files = fs::read_dir(folder_path)
+        .map_err(|e| {
+            eprintln!("Error reading directory: {}", e);
+            Box::new(e) as Box<dyn Error>
+        })
+        .unwrap_or_else(|_| fs::read_dir(".").expect("Fallback read_dir failed")) // Fallback to a default directory or handle it based on your requirements
+        .filter_map(|entry| entry.ok().map(|e| e.path()))
+        .collect::<Vec<_>>();
 
-        files
-            .par_iter()
-            .for_each(|file_path| match exec_script(file_path) {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!(
-                        "\x1b[31mError executing script \x1b[33m{}\x1b[31m: {}\x1b[0m",
-                        file_path.display(),
-                        e
-                    );
-                }
-            });
-    } else {
-        return Err(format!("Error reading directory: {}", folder_path).into());
-    }
+    files.par_iter().for_each(|file_path| {
+        if let Err(e) = exec_script(file_path) {
+            eprintln!(
+                "\x1b[31mError executing script \x1b[33m{}\x1b[31m: {}\x1b[0m",
+                file_path.display(),
+                e
+            );
+        }
+    });
 
     let elapsed_time = start_time.elapsed();
 
     println!(
-        "\x1b[32mFiles successfully executed in \x1b[33m{}\x1b[32m ms!\x1b[0m",
-        elapsed_time.subsec_nanos()
+        "\x1b[32mFiles successfully executed in \x1b[33m{:?}\x1b[32m!\x1b[0m",
+        elapsed_time.as_nanos()
     );
 
     Ok(())
