87bf316e310ed7ff643e83e852a3823d09fbcac9
Yatsu <yatsu.dev@gmail.com>
2023-10-14 22:41:42 +0200

    function calls parsing set up

---
+++ src/parser/function/function_call.rs
@@ -0,0 +1,40 @@
+use crate::parser::{
+    ast::{
+        identifier::{parse_identifier, parse_identifier_expression},
+        Expression,
+    },
+    expression::parse_expression,
+    Span,
+};
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{multispace0, multispace1},
+    combinator::opt,
+    error::VerboseError,
+    multi::separated_list1,
+    IResult,
+};
+
+pub fn parse_fn_call(input: Span) -> IResult<Span, Expression, VerboseError<Span>> {
+    let (input, id) = parse_identifier_expression(input)?;
+
+    let (input, _) = tag("(")(input)?;
+    let (input, args) = opt(separated_list1(tag(","), parse_expression))(input)?;
+
+    let args = if args.is_none() {
+        vec![]
+    } else {
+        args.unwrap()
+    };
+
+    let (input, _) = tag(")")(input)?;
+    let (input, _) = multispace0(input)?;
+
+    let expr = Expression::CallExpression {
+        callee: Box::new(id),
+        args,
+    };
+
+    Ok((input, expr))
+}
